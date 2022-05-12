6c9324a29411eb498dfadc13aae0452c9f65817a
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-05-12 18:27:54 +0300

    added new animation

--- myFlip.js
+++ myFlip.js
@@ -1,16 +1,44 @@
 'use strict';
 gsap.registerPlugin(Flip);
 gsap.config({ trialWarn: false });
+const vw = (coef) => window.innerWidth * (coef/100);
 
+console.log(window.innerWidth * (1/100))
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const containerLeft = document.querySelector(".container__left");
 const containerRight = document.querySelector(".container__right");
 
-const vw = (coef) => window.innerWidth * (coef/100);
+
+const leftCard = document.querySelector(".left-card");
+const rightCard = document.querySelector(".right-card");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+gsap.set(left, {scale: 0, x: vw(-100)});
+gsap.set(right, {scale: 0, x: vw(100)});
+
+leftCard.addEventListener("click", () => {
+	gsap.to(left, {
+		duration: 1, 
+		scale: 1,
+		x: 0
+	});
+})
+
+rightCard.addEventListener("click", () => {
+	gsap.to(right, {
+		duration: 1, 
+		scale: 1,
+		x: 0
+	});
+})
 
 
-let state;
+
+
+
+
 
 // let input = prompt("Введите номер салона сотовой связи")
 let input = '6';
@@ -26,8 +54,16 @@ const qrCode = new QRCodeStyling({
 	image: "./kion.svg",
 	imageSize: "1",
 	dotsOptions: {
-		color: "#4267b2",
+		color: "#C32F77",
 		type: "rounded"
+	},
+	cornersSquareOptions: {
+		color: "#C32F77",
+		type: "dot"
+	},
+	cornersDotOptions: {
+		color: "#C32F77",
+		type: "dot"
 	},
 	backgroundOptions: {
 		color: "transparent",
@@ -49,3 +85,5 @@ function createQr() {
 
 let qr = document.querySelector(".left2__qr");
 qrCode.append(qr);
+
+
